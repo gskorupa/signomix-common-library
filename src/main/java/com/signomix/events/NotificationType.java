0cb546d010cc223035bf89ceeaa28d33de245525
g.skorupa@gmail.com <>
2021-04-05 22:13:47 +0200

    work in progress

---
+++ src/main/java/com/signomix/events/NotificationType.java
@@ -0,0 +1,13 @@
+package com.signomix.events;
+
+/**
+ *
+ * @author greg
+ */
+public class NotificationType {
+    public static final String DEVICE_LOST = "DEVICE_LOST";
+    public static final String INFO = "INFO";
+    public static final String GENERAL = "GENERAL";
+    public static final String WARNING = "WARNING";
+    public static final String ALERT = "ALERT";
+}
---
+++ src/main/java/com/signomix/iot/DeviceStatus.java
@@ -0,0 +1,11 @@
+package com.signomix.iot;
+
+/**
+ *
+ * @author greg
+ */
+public class DeviceStatus {
+    public static final int UNKNOWN = 0;
+    public static final int OK = 1;
+    public static final int FAILURE = 2;
+}
